006ca63dcbb4b266079514f10c2ac29be4f37ff6
StrikerOne65 <golamrabbani20014@gmail.com>
2021-08-24 19:52:29 +0530

    adding service section

--- src/component/About/index.jsx
+++ src/component/About/index.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className='leftAbout'>
           <h3 className='divHeader'>About</h3>
           <h2 className='aboutHeader'>Something goes here second header</h2>
-          <p>
+          <p className='aboutPara'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, hic
             voluptatem. Aspernatur molestiae eaque dolorum nesciunt consequuntur
             aperiam laboriosam dolores, sed asperiores in, quae expedita eius
@@ -17,7 +17,7 @@ const About = () => {
           </p>
         </div>
         <div className='rightAbout'>
-          <img src={aboutImg} alt='about' />
+          <img className='aboutImg' src={aboutImg} alt='about' />
         </div>
       </div>
     </section>
--- src/component/Services/index.jsx
+++ src/component/Services/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
-
+import './services.style.css'
+import Button from '../Button'
+import serviceImg from '../../assets/wedding.svg'
 const Services = () => {
-  return <div></div>
+  return (
+    <section className='service'>
+      <div className='container serviceContainer'>
+        <div className='leftService'>
+          <div className='imageCard'>
+            <img className='aboutImg' src={serviceImg} alt='about' />
+          </div>
+        </div>
+        <div className='rightService'>
+          <h3 className='divHeader'>Services</h3>
+          <h2 className='serviceHeader'>Something goes here second header</h2>
+          <p className='servicePara'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, hic
+            voluptatem. Aspernatur molestiae eaque dolorum nesciunt consequuntur
+            aperiam laboriosam dolores, sed asperiores in, quae expedita eius
+            veniam facilis incidunt saepe?
+          </p>
+          <Button>Register Now</Button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Services
